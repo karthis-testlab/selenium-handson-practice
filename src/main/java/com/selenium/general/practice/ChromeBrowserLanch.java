bff18b87c6750d2c5f851d602458b05ae49e8b9a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-03-24 00:15:39 +0530

    ChromeBrowserLanuch.java file add

---
+++ src/main/java/com/selenium/general/practice/ChromeBrowserLanch.java
@@ -0,0 +1,13 @@
+package com.selenium.general.practice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserLanch {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		driver = new ChromeDriver();
+	}
+
+}
