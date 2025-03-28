3fac6f80bbee52825139db5251e9c12412b7ee59
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-03-29 00:01:36 +0530

    29032025 12:01AM

--- src/main/java/com/selenium/general/practice/FullPageScreenShot.java
+++ src/main/java/com/selenium/general/practice/FullPageScreenShot.java
@@ -1,10 +1,12 @@
 package com.selenium.general.practice;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FullPageScreenShot {
 
 	public static void main(String[] args) {
 		
-		
+		ChromeDriver driver = new ChromeDriver(); 
 
 	}
 
