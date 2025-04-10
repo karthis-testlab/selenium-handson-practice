f0f54d671eb8dd48361bd8ebe388530394f68dec
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-04-10 20:20:23 +0530

    10042025 08:20PM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -1,9 +1,11 @@
 package com.selenium.general.practice;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicAuthHandler {
 	
 	public static void main(String[] args) {
-		
+		ChromeDriver driver = new ChromeDriver();
 	}
 
 }
