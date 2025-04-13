9c958c02d9f6d1e25f9952e4606759a534970041
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-04-13 23:04:15 +0530

    13042025 11:04PM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -7,6 +7,7 @@ public class BasicAuthHandler {
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("");
 	}
 
 }
