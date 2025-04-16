afa67cf28569eafb1ae78f5a3887cf2dc822d8c9
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-04-17 01:10:44 +0530

    17042025 01:10AM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -3,10 +3,12 @@ package com.selenium.general.practice;
 import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasicAuthHandler {
 	
 	public static void main(String[] args) {
+		ChromeOptions options = new ChromeOptions();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.browserstack.com/guide/selenium-scroll-tutorial");
