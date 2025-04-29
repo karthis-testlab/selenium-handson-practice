5668b54ae9e68f7c49fb883c7727ed499e94d41f
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-04-30 01:42:50 +0530

    30042025 01:42AM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -9,7 +9,7 @@ public class BasicAuthHandler {
 	
 	public static void main(String[] args) {
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--started-");
+		options.addArguments("--start-maximized");
 		ChromeDriver driver = new ChromeDriver();		
 		driver.get("https://www.browserstack.com/guide/selenium-scroll-tutorial");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
