43856882d7ad6a5ae4f11d6dd6db67e41bef5da0
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-05-14 00:51:04 +0530

    14052025 12:51AM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -2,6 +2,7 @@ package com.selenium.general.practice;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,7 +16,7 @@ public class BasicAuthHandler {
 		driver.register(UsernameAndPassword.of("admin", "admin"));
 		driver.get("https://the-internet.herokuapp.com/basic_auth");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		System.out.println(driver.findElement(""));
+		System.out.println(driver.findElement(By.tagName(""));
 	}
 
 }
