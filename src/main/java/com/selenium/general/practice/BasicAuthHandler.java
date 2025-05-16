8fc31d3807b376dfcc188eab1e7e0aa01a8e3cd1
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-05-17 00:13:25 +0530

    17052025 12:13AM

--- src/main/java/com/selenium/general/practice/BasicAuthHandler.java
+++ src/main/java/com/selenium/general/practice/BasicAuthHandler.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasicAuthHandler {
 	
+	static ChromeDriver driver;
+	
 	public static void main(String[] args) {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
-		ChromeDriver driver = new ChromeDriver(options);
+	    driver = new ChromeDriver(options);
 		driver.register(UsernameAndPassword.of("admin", "admin"));
 		driver.get("https://the-internet.herokuapp.com/basic_auth");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
